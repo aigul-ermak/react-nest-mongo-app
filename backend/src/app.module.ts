87f7a5cf2aaa6e3ec2673af478ff3c4b37fedb08
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-08 17:55:50 -0400

    chore(core): update app.module.ts configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {ConfigModule} from "@nestjs/config";
+import { BlogModule } from './blog/blog.module';
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    BlogModule,
+    UserModule,
+    PostModule,
   ],
   controllers: [AppController],
   providers: [AppService],
