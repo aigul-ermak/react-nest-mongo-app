3a8bec8e015370a2214b3205ba4cf73982b0d4bb
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-08 16:45:41 -0400

    Created projects for backend and frontend

---
+++ backend/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {ConfigService} from "@nestjs/config";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+
+  const configService = app.get(ConfigService);
+
+
+  const port = configService.get<number>('APP_PORT') || 3000;
+
+  console.log(`🚀 App running on port: ${port}`);
+
+  await app.listen(port);
+}
+bootstrap();
