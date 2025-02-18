7b135dcb60947abf9549f9805a92ec5241cefc4c
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-18 13:46:31 -0400

    feat(user): added decorator for validate email

---
+++ backend/src/basics/validation/isOptionalEmail.decorator.ts
@@ -0,0 +1,5 @@
+import {applyDecorators} from "@nestjs/common";
+import {IsEmail, IsOptional} from "class-validator";
+import {Trim} from "../transform/trim";
+
+export const IsOptionalEmail = () => applyDecorators(IsEmail(), Trim(), IsOptional());
