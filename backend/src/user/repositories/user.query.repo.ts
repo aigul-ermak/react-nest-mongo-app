33e3b054a939e475c96ad4b7a01343e0a084aea0
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-18 20:19:31 -0400

    feat(user): updated repos for user

--- backend/src/user/repositories/user.query.repo.ts
+++ backend/src/user/repositories/user.query.repo.ts
@@ -1,10 +1,8 @@
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
-import {User} from "../entities/user.entity";
-import {UserDocument} from "../entities/user.schema";
+import {User, UserDocument} from "../entities/user.schema";
 import {UserMapper} from "../dto/mapper/user.mapper";
 import {UserLoginMapper} from "../dto/mapper/user-login.mapper";
-
 
 export class UserQueryRepo {
     constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {
@@ -16,11 +14,9 @@ export class UserQueryRepo {
 
     async findOne(id: string) {
         const user = await this.userModel.findById(id).exec();
-
         if (!user) {
             return null;
         }
-
         return UserMapper(user);
     }
 
--- backend/src/user/repositories/user.repo.ts
+++ backend/src/user/repositories/user.repo.ts
@@ -1,7 +1,6 @@
 import {InjectModel} from "@nestjs/mongoose";
 import {Model} from "mongoose";
-import {User} from "../entities/user.entity";
-import {UserDocument} from "../entities/user.schema";
+import {User, UserDocument} from "../entities/user.schema";
 import {CreateUserDto} from "../dto/create-user.dto";
 
 
