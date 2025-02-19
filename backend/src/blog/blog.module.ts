5e45ca2108739222484b89fb8d31914ef4d487d7
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-18 20:22:20 -0400

    feat(blog): updated blog module

--- backend/src/blog/blog.module.ts
+++ backend/src/blog/blog.module.ts
@@ -2,13 +2,13 @@ import {Module} from '@nestjs/common';
 import {BlogService} from './blog.service';
 import {BlogController} from './blog.controller';
 import {MongooseModule} from "@nestjs/mongoose";
-import {Blog} from "./entities/blog.entity";
-import {BlogSchema} from "./entities/blog.schema";
+import {Blog, BlogSchema} from "./entities/blog.schema";
 import {BlogRepo} from "./repositories/blog.repo";
 import {BlogQueryRepo} from "./repositories/blog.query.repo";
 
 @Module({
-    imports: [MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}])],
+    imports: [MongooseModule.forFeature([{name: Blog.name, schema: BlogSchema}]),
+    BlogModule],
     controllers: [BlogController],
     providers: [BlogService, BlogRepo, BlogQueryRepo],
     exports: [BlogRepo, BlogQueryRepo],
