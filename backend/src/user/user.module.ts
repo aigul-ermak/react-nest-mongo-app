bafd39e1058d4f2493512c81a690e3c49c7af835
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-18 20:19:14 -0400

    feat(user): update user controller

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -2,8 +2,7 @@ import {Module} from '@nestjs/common';
 import {UserService} from './user.service';
 import {UserController} from './user.controller';
 import {MongooseModule} from "@nestjs/mongoose";
-import {User} from "./entities/user.entity";
-import {UserSchema} from "./entities/user.schema";
+import {User, UserSchema} from "./entities/user.schema";
 import {UserRepo} from "./repositories/user.repo";
 import {UserQueryRepo} from "./repositories/user.query.repo";
 
@@ -11,7 +10,7 @@ import {UserQueryRepo} from "./repositories/user.query.repo";
     imports: [MongooseModule.forFeature([{name: User.name, schema: UserSchema}])],
     controllers: [UserController],
     providers: [UserService, UserRepo, UserQueryRepo],
-    exports: [UserRepo, UserQueryRepo],
+    exports: [UserRepo, UserQueryRepo, MongooseModule],
 })
 export class UserModule {
 }
