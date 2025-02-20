6449d3661a50b39f0475380f2122058c5ec15c6a
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-19 22:12:07 -0400

    added files

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import * as React from 'react';
 import './App.css'
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {Header} from "./components/Header.tsx";
+import {Home} from "./components/Home.tsx";
+import {Box} from "@mui/material";
+import {Register} from "./context/Register.tsx";
+import {Footer} from "./components/Footer.tsx";
+import {Login} from "./components/Login.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+
+const App = () => {
+    return (
+        <Router>
+            <Box display="flex" flexDirection="column" minHeight="100vh">
+                <Header />
+                <Box flexGrow={1}>
+                    <Routes> {}
+                        <Route path="/" element={<Home />} />
+                        {/*<Route path="/login" element={<Login />} />*/}
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
+                </Box>
+                <Footer />
+            </Box>
+        </Router>
+    );
+};
 
 export default App
