258afe520ce5d0b6cea34bae6b21217d6173244a
Aigul <69362971+aigul-ermak@users.noreply.github.com>
2025-02-21 11:34:26 -0400

    Updated base url

--- frontend-app/src/api/api.ts
+++ frontend-app/src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000";
+// const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://react-nest-mongo-app.onrender.com/";
 
 export const api = axios.create({
     baseURL: API_BASE_URL,
